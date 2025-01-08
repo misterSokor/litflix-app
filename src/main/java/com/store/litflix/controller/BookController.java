8673ba53c87b94177846ca77aa7a101c3da34e72
mister.Sokor <mister.sokor@gmail.com>
2025-01-08 13:26:14 -0600

    project is ready for the code review: Create the  entity, Add necessary DTOs and a mapper, Add , Add  annotation, modify  for handling

--- src/main/java/com/store/litflix/controller/BookController.java
+++ src/main/java/com/store/litflix/controller/BookController.java
@@ -54,18 +54,17 @@ public class BookController extends HttpServlet {
     @Operation(summary = "finds books by specific criteria",
             description = "finds books by specific criteria")
     public List<BookDto> searchBooks(@RequestParam(required = false) String[] title,
-                                     @RequestParam(required = false) String[] author,
-                                     Pageable pageable) {
+                                     @RequestParam(required = false) String[] author) {
         BookSearchParametersDto bookSearchParametersDto =
                 new BookSearchParametersDto(title, author);
-        return bookService.search(bookSearchParametersDto, pageable);
+        return bookService.search(bookSearchParametersDto);
     }
 
     @PutMapping("/{id}")
     @Operation(summary = "method is used to update existing book",
             description = "method is used to update existing book")
     public BookDto updateBook(@PathVariable Long id,
-                              @RequestBody @Valid UpdateBookRequestDto requestDto) {
+                              @RequestBody UpdateBookRequestDto requestDto) {
         return bookService.updateBook(id, requestDto);
     }
 
